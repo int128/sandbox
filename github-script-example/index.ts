33d2fb81f1d8eb3d48b520be62e282baeab7d752
Hidetake Iwata <int128@gmail.com>
2026-07-17 18:56:00 +0900

    Use @actions/github

--- github-script-example/index.ts
+++ github-script-example/index.ts
@@ -1,3 +1,16 @@
-export const main = ({ core }: { core: typeof import('@actions/core') }) => {
+type GitHubScriptContext = {
+  core: typeof import('@actions/core')
+  context: typeof import('@actions/github').context
+  github: ReturnType<typeof import('@actions/github').getOctokit>
+}
+
+export const main = async ({ core, context, github }: GitHubScriptContext) => {
   core.info('Hello from TypeScript!')
+
+  await github.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    body: 'Hello from TypeScript!'
+  })
 }
